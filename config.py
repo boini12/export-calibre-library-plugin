b7a31f0260be9978fc1bc44f31b506554049c48a
Ines Bohr <bohrines@gmail.com>
2025-10-26 17:01:46 +0100

    Improved config dialog

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from calibre.utils.config import JSONConfig
 
-from qt.core import QLabel, QWidget, QLineEdit, QCheckBox, QVBoxLayout
+from qt.core import QLabel, QWidget, QLineEdit, QCheckBox, QVBoxLayout, QHBoxLayout
 
 from .constant import SETTINGS_KEY_DESTINATION, SETTINGS_KEY_AUTOMATIC, DEFAULT_BACKUP_DESTINATION
 
@@ -12,24 +12,32 @@ prefs.defaults[SETTINGS_KEY_AUTOMATIC] = False
 class ConfigWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        self.l = QVBoxLayout()
-        self.setLayout(self.l)
+        self.outer_layout = QVBoxLayout()
+        self.setLayout(self.outer_layout)
+
+        self.destination_layout = QHBoxLayout()
 
         self.label_destination = QLabel("Backup Destination:")
-        self.l.addWidget(self.label_destination)
+        self.destination_layout.addWidget(self.label_destination)
 
         self.msg = QLineEdit()
         self.msg.setText(prefs[SETTINGS_KEY_DESTINATION])
-        self.l.addWidget(self.msg)
+        self.destination_layout.addWidget(self.msg)
         self.label_destination.setBuddy(self.msg)
 
-        self.auto_backup_label = QLabel(" (Export new books each time Calibre is closed)")
-        self.l.addWidget(self.auto_backup_label)
+        self.outer_layout.addLayout(self.destination_layout)
+
+        self.auto_layout = QHBoxLayout()
+
+        self.auto_backup_label = QLabel("Export new books each time Calibre is closed:")
+        self.auto_layout.addWidget(self.auto_backup_label)
 
         self.checked = QCheckBox()
         self.checked.setChecked(prefs[SETTINGS_KEY_AUTOMATIC])
-        self.l.addWidget(self.checked)
+        self.auto_layout.addWidget(self.checked)
         self.auto_backup_label.setBuddy(self.checked)
+
+        self.outer_layout.addLayout(self.auto_layout)
 
     def save_settings(self):
         prefs[SETTINGS_KEY_DESTINATION] = self.msg.text()
